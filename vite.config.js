7aed253455167d6235be03c4da9a1f2465d881d1
ClideHiraji <kingclidejagape122770@gmail.com>
2025-10-22 20:18:11 +0800

    Add files all

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
+import { defineConfig } from 'vite'
+
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
-    },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
-}
+export default defineConfig({
+  root: 'src',                  
+  publicDir: '../static',       
+  base: process.env.NODE_ENV === 'production'
+    ? '/Jagape-ITE18-Act1.10/' 
+    : '/',
+  server: {
+    host: true,
+    open: !isCodeSandbox
+  },
+  build: {
+    outDir: '../dist',           
+    emptyOutDir: true,
+    sourcemap: true
+  }
+})
