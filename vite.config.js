e81bcc3d5e94dfe0f4e82ce86c09b1334ef36534
ClideHiraji <kingclidejagape122770@gmail.com>
2025-10-22 20:18:44 +0800

    Add files all

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   root: 'src',                  
   publicDir: '../static',       
   base: process.env.NODE_ENV === 'production'
-    ? '/Jagape-ITE18-Act1.10/' 
+    ? '/Jagape-ITE18-Act2.2/' 
     : '/',
   server: {
     host: true,
